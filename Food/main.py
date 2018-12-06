f610a6b908a31f9b67362ec31bacd213c55315f3
λ <skandix@datapor.no>
2018-12-06 22:56:10 +0100

    Update main.py

--- Food/main.py
+++ Food/main.py
@@ -4,7 +4,6 @@ def getData(url):
     noPromo={'app-promo':'yes'}
     return requests.get(url, cookies=noPromo).json()
 
-
 def foodCategory(cat="godis"):
     offer = getData((base64.b64decode('aHR0cHM6Ly9hcGkubWF0dGlsYnVkLmNvbS92MS9vZmZlcnMvYXVzdGFnZGVyL2NhdGVnb3JpZXM=').decode('utf-8')))
 
@@ -13,7 +12,4 @@ def foodCategory(cat="godis"):
         if cat in title:
             return (element['itemsUrl'])
 
-
-
 print (foodCategory())
-
