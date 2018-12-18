105c5678272ac10120528500dcd4e81eab81551a
Bendik λ <skandix@datapor.no>
2018-12-18 16:05:01 +0100

    Update main.py

--- Food/main.py
+++ Food/main.py
@@ -4,12 +4,31 @@ def getData(url):
     noPromo={'app-promo':'yes'}
     return requests.get(url, cookies=noPromo).json()
 
-def foodCategory(cat="godis"):
+def foodCategory(cat=""):
     offer = getData((base64.b64decode('aHR0cHM6Ly9hcGkubWF0dGlsYnVkLmNvbS92MS9vZmZlcnMvYXVzdGFnZGVyL2NhdGVnb3JpZXM=').decode('utf-8')))
+    avaliableID = ["husholdning", "hygiene", "kjottdeig", "kyllingfilet", "fisk", "ost", "ferdigmat", "brus", "godis", "kaffe"]
 
-    for element in offer['categories']:
-        title = element['itemsUrl'].split('/')[-2]
-        if cat in title:
-            return (element['itemsUrl'])
+    if cat == "":
+    	return ("please give me a valid id ", avaliableID)
 
-print (foodCategory())
+    if cat in avaliableID:
+	    for element in offer['categories']:
+	        title = element['itemsUrl'].split('/')[-2]
+	        if cat in title:
+	            return (element['itemsUrl'])
+
+def foodStore(cat=""): #denne funker ikke helt ordentlig enda, use with caution
+    offer = getData((base64.b64decode('aHR0cHM6Ly9hcGkubWF0dGlsYnVkLmNvbS92MS9vZmZlcnMvYXVzdGFnZGVyL3N0b3Jlcw==').decode('utf-8')))
+    avaliableID = ["husholdning", "hygiene", "kjottdeig", "kyllingfilet", "fisk", "ost", "ferdigmat", "brus", "godis", "kaffe"]
+
+    if cat == "":
+    	return ("please give me a valid id ", avaliableID)
+
+    if cat in avaliableID:
+	    for element in offer['categories']:
+	        title = element['title'].split('/')[-2]
+	        if cat in title:
+	            return (element['title'])
+
+if __name__ == '__main__':
+	print (foodCategory(""))
